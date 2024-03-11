2074b4f2c21195d83f5e08bac1f438feab884179
mahnoor faisal <faisalmahnoor86@gmail.com>
2024-03-11 21:26:45 +0500

    items adding..

--- App.tsx
+++ App.tsx
@@ -5,15 +5,32 @@
  * @format
  */
 
-// import {Typography} from '@mui/material';
 import React from 'react';
-import {Button, SafeAreaView, StyleSheet, Text, TextInput} from 'react-native';
+import {
+  Button,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+//import {v4 as uuidv4} from 'uuid';
 
 function App(): React.JSX.Element {
   const [text, onChangeText] = React.useState('');
-
+  const [data, setData] = React.useState<string[]>([]);
+  //const [id, setId] = React.useState<string>('1');
   const Add = () => {
-    console.log('adding');
+    if (text.trim() !== '') {
+      setData(prevData => [...prevData, text]);
+      onChangeText('');
+      console.log('printiing');
+      //setId(uuidv4());
+    }
+  };
+  const Delete = () => {
+    //console.log(id);
   };
 
   return (
@@ -25,11 +42,27 @@ function App(): React.JSX.Element {
         onChangeText={onChangeText}
         value={text}
       />
-      <Button
-        onPress={Add}
-        title="Add"
-        color="green"
-        accessibilityLabel="Learn more about this purple button"
+      <View style={styles.button}>
+        <Button onPress={Add} title="Add" color="gray" />
+      </View>
+      <FlatList
+        key={data.length}
+        data={data}
+        renderItem={({item}) => (
+          <View style={styles.container}>
+            <Text style={styles.list}>
+              {item}
+              <View style={styles.actionButton}>
+                <Button onPress={Delete} title="Delete" color="gray" />
+                <Button
+                  onPress={() => console.log('Button for item:', item)}
+                  title="Edit"
+                  color="gray"
+                />
+              </View>
+            </Text>
+          </View>
+        )}
       />
     </SafeAreaView>
   );
@@ -37,19 +70,40 @@ function App(): React.JSX.Element {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
+    display: 'flex',
+    flex: 1,
   },
   title: {
-    display: 'flex',
     marginTop: 130,
     fontSize: 30,
+    textAlign: 'center',
+  },
+  button: {
+    width: 80,
+    fontSize: 90,
+    marginBottom: 15,
+    marginLeft: 30,
   },
   input: {
-    height: 40,
+    height: 50,
     width: 300,
     margin: 30,
     borderWidth: 1,
     padding: 10,
+  },
+  list: {
+    fontSize: 20,
+    margin: 10,
+    width: 300,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 10,
+    padding: 10,
+    alignSelf: 'center',
+  },
+  actionButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
 });
 
